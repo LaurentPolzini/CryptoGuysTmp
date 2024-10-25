a428dcf19abf9cde30d793df3afb7c879fac19a4
LaurentPolzini <laurentpolzin@gmail.com>
2024-10-26 00:43:22 +0200

    euh il manquait des fichiers

--- Partie_1/xor.h
+++ Partie_1/xor.h
@@ -1,7 +1,7 @@
 #ifndef __XOR_H__
 #define __XOR_H__
 
-int encrypt_decrypt_xor(char *file_in, char *file_key, char *file_out);
+char *encrypt_decrypt_xor(char *file_in, char *file_key, char *file_out);
 
 
 #endif
